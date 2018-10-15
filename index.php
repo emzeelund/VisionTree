2032fd03d6a24186561c4a2bffa858e106b8df28
Mark Lund <marklund14@yahoo.com>
2018-10-15 13:30:54 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<htmL>
+<head>
+  <title>Banner Sample</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../VisionTree/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../VisionTree/css/customized.css">
+  <script src="../VisionTree/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item item1 active">
+        <div class="container banner-object">
+          <div class="col-md-3">test</div>
+          <div class="col-md-3">test</div>
+          <div class="col-md-3">test</div>
+          <div class="col-md-3">test</div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="../VisionTree/img/home_main.jpg" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="../VisionTree/img/home_main.jpg" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+</body>
+</html>
